a70bb312af7d1e23a130316979057e14650eacdd
Steven Harlow <stharlow@gmail.com>
2022-08-31 23:25:05 +0200

    remove other branding

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Contentful Starter',
-    description: 'Official Contentful Gatsby Starter',
+    title: 'HarlowCarbonell Personal Website',
+    description: 'Website for the projects of Steven and Ciara',
   },
   plugins: [
     'gatsby-transformer-sharp',
